72b63f0d74826df398a44189d427991b77eddde2
Michael Tran <michaelhuytran@gmail.com>
2018-02-13 21:22:15 -0800

    [feature] - quiz

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {
-	AppRegistry,
 	StyleSheet,
 	Text,
 	View,
@@ -8,51 +7,99 @@ import {
 	Animated
 } from 'react-native';
 
-class Quiz extends Component() {
-	componentDidMount() {
+class Quiz extends Component {
+	componentWillMount() {
 		this.animatedValue = new Animated.Value(0);
 		this.value = 0;
-
 		this.animatedValue.addListener(({ value }) => {
 			this.value = value;
-		});
-
+		})
 		this.frontInterpolate = this.animatedValue.interpolate({
-			inputRange: [0 , 180],
+			inputRange: [0, 180],
 			outputRange: ['0deg', '180deg'],
-		});
-
+		})
 		this.backInterpolate = this.animatedValue.interpolate({
-			inputRange: [0 , 180],
-			outputRange: ['180deg', '0deg'],
-		});
+			inputRange: [0, 180],
+			outputRange: ['180deg', '360deg']
+		})
 	}
-
-	flipCard = () => {
-		if(this.value >= 90) {
+	flipCard() {
+		if (this.value >= 90) {
 			Animated.spring(this.animatedValue, {
 				toValue: 0,
 				friction: 8,
-				tension: 10,
-			}).start()
+				tension: 10
+			}).start();
 		} else {
-			Animated.spring(this.animatedValue,{
+			Animated.spring(this.animatedValue, {
 				toValue: 180,
 				friction: 8,
 				tension: 10
 			}).start();
 		}
-	};
+	}
 
 	render() {
+		const frontAnimatedStyle = {
+			transform: [
+				{ rotateY: this.frontInterpolate}
+			]
+		};
+		const backAnimatedStyle = {
+			transform: [
+				{ rotateY: this.backInterpolate }
+			]
+		};
 		return (
-			<View>
-
-
+			<View style={styles.container}>
+				<TouchableOpacity onPress={() => this.flipCard()}>
+					<Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+						<Text style={styles.flipText}>
+							Question
+						</Text>
+					</Animated.View>
+					<Animated.View style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
+						<Text style={styles.flipText}>
+							Answer
+						</Text>
+					</Animated.View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => this.flipCard()}>
+					<Text>Flip!</Text>
+				</TouchableOpacity>
 			</View>
-		)
+		);
 	}
-};
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	flipCard: {
+		width: 400,
+		height: 400,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'blue',
+		backfaceVisibility: 'hidden',
+		borderRadius
+	},
+	flipCardBack: {
+		backgroundColor: "red",
+		position: "absolute",
+		top: 0,
+	},
+	flipText: {
+		width: 90,
+		fontSize: 20,
+		color: 'white',
+		fontWeight: 'bold',
+	}
+});
+
 
 export default Quiz;
 
